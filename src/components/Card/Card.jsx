3131584353a17ea73bc2a0ad7a88818352b24806
Alexey-Glazov <44892903+Alexey-Glazov@users.noreply.github.com>
2023-10-26 09:13:10 +0300

    add timeout to remember cards on start

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,20 @@
 import "./Card.scss";
 import { memo, useState } from "react";
 import cn from "classnames";
+import { useEffect } from "react";
 const Card = memo(({ url, cardId }) => {
-  const [CardOpen, setCardOpen] = useState(false);
+  const [CardOpen, setCardOpen] = useState(true);
+  useEffect(() => {
+    console.log(CardOpen);
+    const timer = setTimeout(() => {
+      setCardOpen(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleCardClick = () => {
+    setCardOpen(!CardOpen);
+  };
 
   const overlayClassNames = cn("overlay", { clicked: CardOpen });
   return (
@@ -16,7 +28,7 @@ const Card = memo(({ url, cardId }) => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className={overlayClassNames} onClick={() => setCardOpen(!CardOpen)}>
+      <div className={overlayClassNames} onClick={handleCardClick}>
         {cardId}
       </div>
     </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,46 @@
 import { memo } from "react";
 import "./MainPage.scss";
 import GameField from "../components/GameField/GameField";
+import { useState } from "react";
 
 const MainPage = memo(() => {
+  const [startPressed, setStartPressed] = useState(false);
+  const [gameFieldConfig, setGameFieldConfig] = useState(null);
+  console.log(startPressed);
+  const handleStart = (gridSize) => {
+    setStartPressed(true);
+    setGameFieldConfig(gridSize);
+  };
+
   return (
     <>
       <header className='header'>
         <h1 className='header-title'>Найди пару</h1>
       </header>
+      <aside className='left-sidebar'>
+        <button onClick={() => handleStart("small")} className='start'>
+          Легко
+        </button>
+        <button onClick={() => handleStart("medium")} className='start'>
+          Средне
+        </button>
+        <button onClick={() => handleStart("large")} className='start'>
+          Сложно
+        </button>
+      </aside>
       <main className='main-content'>
-        <GameField size='medium' />
+        {startPressed && <GameField size={gameFieldConfig} />}
       </main>
+      <aside className='right-sidebar'>очки:</aside>
       <footer className='footer'>
         <p className='footer-text'>Alexey Glazov 2023 ©</p>
       </footer>
     </>
   );
 });
+
+{
+  /* <GameField size='medium' /> */
+}
 
 export default MainPage;
